47cf0cb9fee95533d5b90a38ce9e5239f1217628
Veselin Hr. Trayanov <1.1@gmx.com>
2023-05-01 21:32:09 +0100

    name="viewport" ++

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c62834c1c48b9da9"; }
+/******/ 		__webpack_require__.h = function() { return "359af33f676e2a85"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
